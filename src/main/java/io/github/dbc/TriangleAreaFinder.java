16a29ea7f7666c75f7d661ba0fa5aec5759acb14
dbc2201 <divyanshb22@gmail.com>
2022-06-19 18:34:01 +0530

    create the method to be completed

---
+++ src/main/java/io/github/dbc/TriangleAreaFinder.java
@@ -0,0 +1,16 @@
+package io.github.dbc;
+
+public class TriangleAreaFinder {
+    /**
+     * This method calculates the area of a triangle based on the following formula.
+     * See the formula here: <a href="https://en.wikipedia.org/wiki/Heron's_formula">Heron's Formula</a>
+     *
+     * @param side1 the length of the first side of the triangle.
+     * @param side2 the length of the second side of the triangle.
+     * @param side3 the length of the third side of the triangle.
+     * @return the calculated area of the triangle, OR 0 if any side less than or equal to 0.
+     */
+    public double calculateAreaOfTriangleUsingHeronsFormula(double side1, double side2, double side3) {
+        return Double.MIN_VALUE;
+    }
+}
